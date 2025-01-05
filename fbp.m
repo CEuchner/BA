3ea02a36d9732e8abc5cf2ea20ffb81e7d8a4da5
CEuchner <cheuchner@gmx.de>
2025-01-05 17:27:03 +0100

    modified fbp to also work for Images of uneven size

--- fbp.m
+++ fbp.m
@@ -1,5 +1,5 @@
 function Image = fbp(Sinogram, angs, useFilter, usePadding)
-% Calculating filtered backprojection with a given Sinogram
+% Calculating filtered backprojection with a given Sinogram and angles
 %
 %   Input Arguments
 %       Sinogram    input Sinogram as 2D-Image
@@ -17,28 +17,27 @@ function Image = fbp(Sinogram, angs, useFilter, usePadding)
 
 projections = Sinogram;
 
+% decicde if sinogram gets padded
 if usePadding == true
     N = size(projections, 1);
     projections = padarray(projections, N);
 end
 
 % get the number and length of projections
-
 M = size(projections, 2);
 N = size(projections, 1);
 
-% in order to rotate around the right center pixels must be added
+% in order to create the correct RamLak-filter get a pixel shift for even 
+% projection lengths 
 if mod(N, 2) == 0
     add_f = 0;
 else
-    add_f = -.5;
+    add_f = .5;
 end
 
 % define filter function (ramp-filter) for weighing sliecs in k-space
 if useFilter == true
     filter = abs(-N/2+add_f:N/2-1+add_f)';
-    % filter = fftshift(ifft(ifftshift(RamLak(N/2)))).';
-    % filter = real(filter(2:end));
 else
     filter = ones([N 1]);
 end
@@ -46,14 +45,14 @@ end
 % define Image to be filled with backprojections
 Image = zeros(N, N);
 
-projections = fftshift(fft(ifftshift(projections)));
+% 1D-fourier-transform the projections
+% use the filter on each fourier-transmormed projection
+% and 1D-inverse-fourier-transform the filtered and transformed projections
+projections = fft(projections);
+projections = circshift(projections, N/2+add_f, 1);
 projections = projections .* repmat(filter, 1, M);
-projections = ifftshift(ifft(fftshift(projections)));
-
-% zeropad sinogram to get projections on sqrt(2) times the size
-fac = 0;%N;%ceil(size(projections, 2) * (sqrt(2) - 1) / 2)*1;
-% projections = padarray(projections, fac);
-N2 = size(projections, 1);
+projections = circshift(projections, -int16(N/2+add_f), 1);
+projections = ifft(projections);
 
 % each loop is a backprojection in one angle
 for kk = 1:numel(angs)
@@ -61,13 +60,12 @@ for kk = 1:numel(angs)
     % create backprojection in angele 0,
     % add pixel for it to be centered
     % and rotate it to its actual angle position
-    backproj = repmat(projections(:,kk), 1, N2);
+    backproj = repmat(projections(:,kk), 1, N);
     backproj(end, end) = 0;
     backproj = imrotate(backproj, 90+angs(kk), 'bicubic', 'crop');
-    % backproj = fourierRotate(backproj, 90+angles(kk));
-
+    
     % add backprojection to image
-    Image = Image + backproj(fac+1:N2-fac, fac+1:N2-fac);
+    Image = Image + backproj(1:N, 1:N);
 
 end %for
 
